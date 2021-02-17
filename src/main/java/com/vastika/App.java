fddc48ca26870da35cba52290b545cd81677da28
kcabhishek <abhi888kc@gmail.com>
2021-02-16 20:21:35 -0600

    Added App.java and User.java

--- src/main/java/com/vastika/App.java
+++ src/main/java/com/vastika/App.java
@@ -1,5 +1,17 @@
 package com.vastika;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Properties;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +20,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration();
+        Properties props = new Properties();
+        try {
+            props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+            configuration.setProperties(props);
+            configuration.addAnnotatedClass(User.class);
+            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+
+            User user = new User();
+            user.setUsername("test1");
+            user.setPassword("pp");
+            user.setDob(LocalDate.now());
+            user.setAddress("Kathmandu");
+            session.save(user);
+            session.getTransaction().commit();
+            session.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
